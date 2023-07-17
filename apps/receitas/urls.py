0e3182e984b95e1bb2cc498518d6210bb834f2d8
Germano B. Nascimento <nascimento.germano@gmail.com>
2023-07-17 14:44:54 -0300

    Pagina de receitas, completo

---
+++ apps/receitas/urls.py
@@ -0,0 +1,22 @@
+"""from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('appreceita', views.appreceita, name='appreceita')
+]"""
+
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<int:receita_id>', receita, name='receita'),
+    path('buscar', buscar, name='buscar'),
+    path('cria/receita', cria_receita, name='cria_receita'),
+    path('deleta/<int:receita_id>', deleta_receita, name='deleta_receita'),
+    path('edita/<int:receita_id>', edita_receita, name='edita_receita'),
+    path('atualiza_receita', atualiza_receita, name='atualiza_receita')
+]
